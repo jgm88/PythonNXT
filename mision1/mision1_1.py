9f1d08f5dff826901f05e12b8a4fd2e10db2379f
chancla15 <jesusmanresa1@gmail.com>
2015-04-27 11:54:44 +0200

    Finish mision1

--- mision1/mision1_1.py
+++ mision1/mision1_1.py
@@ -10,40 +10,53 @@ class Robot:
     def __init__(self, brick, tam_encoder=360, wheel_diameter=5.6):
         
         self.brick_= brick
-        self.separationBetweenWheels_= 8
+        self.separationBetweenWheels_= 13
         self.syncMotor_ = SynchronizedMotors(Motor(self.brick_, PORT_B), Motor(self.brick_, PORT_C), 0)
-        self.cuenta_= ((encoder_*math.pi)/wheelDiameter_)
+        self.cuenta_= ((wheel_diameter*math.pi)/tam_encoder)
 
         # 1. Calculamos las cuentas que tendra que pasar para girar hacia un stolado.
         # Si suponemos que un giro sobre si mismo es de de radio separationBewteenWheels, un giro solo ocupara una
         # cuarta parte del perimetro de la circunferencia.
-        turn_perimeter = (math.pi * self.wheelDiameter_) / 4
-        self.cuentasGiro = turn_perimeter / self.cuenta_
+        turn_perimeter = (math.pi * 2.0 * self.separationBetweenWheels_) / 4.0
+        self.cuentasGiro_ = turn_perimeter / self.cuenta_
 
-    def moveTurnTime(self,time):
-         current_time = time.time()
+    def moveTurnTime(self,t):
+
+        current_time = time.time()
         self.syncMotor_.run(80)
-        while (time.time() - current_time < time):
+        while (time.time() - current_time < t):
             pass;      
-        sync.brake()
+        self.syncMotor_.brake()
 
     def mision(self):
 
+
         # 1.Mover robot hacia delante 3 segundos
-        moveTurnTime(3.0)
-
+        self.moveTurnTime(3.0)
         # 2.Girar a la derecha
-        self.syncMotor_.follower.weak_turn(80, self.cuentasGiro)
-
+       # print "init: ", self.syncMotor_.follower.get_tacho()
+        time.sleep(1)
+        self.syncMotor_.leader.weak_turn(80, self.cuentasGiro_)
+        time.sleep(1)
+        
+        #print "final: ", self.syncMotor_.follower.get_tacho()
 
         # 3.Mover robot hacia delante 2 segundos
-        moveTurnTime(2.0)
+        self.moveTurnTime(2.0)
+        time.sleep(1)
 
         # 4. Girar a la izquierda
-        self.syncMotor_.leader.weak_turn(80, self.cuentasGiro)
+        self.syncMotor_.follower.weak_turn(80, self.cuentasGiro_)
+        time.sleep(1)
 
-        # 5. Moverse hacia delante 1 segundos
-        moveTurnTime(1.0)
+        # 5. Moverse hacia delante 3 segundos
+        self.moveTurnTime(3.0)
+
+        self.syncMotor_.follower.weak_turn(80, self.cuentasGiro_)
+        time.sleep(1)
+
+        self.moveTurnTime(3.0)
+        time.sleep(1)
 
         # 6. Dessincrnizacion motores
         self.syncMotor_.idle()
@@ -52,6 +65,6 @@ class Robot:
         self.brick_.play_tone_and_wait(659, 500)
 
 if __name__=='__main__':
-    #robot= Robot(nxt.locator.find_one_brick())
-    robot= Robot(nxt.bluesock.BlueSock('00:16:53:09:46:3B').connect())
+    robot= Robot(nxt.locator.find_one_brick())
+    #robot= Robot(nxt.bluesock.BlueSock('00:16:53:09:46:3B').connect())
     robot.mision()
