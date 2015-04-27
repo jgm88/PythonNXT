0d198a7fa725f505e1aab4b85413a7cd792233a2
yizu <yizu@blackfingers.local>
2015-04-27 19:48:21 +0200

    Añadido script de ultrasonidos

--- README.txt
+++ README.txt
@@ -1,6 +1,15 @@
 AUTORES:
 
-MISIONES COMPLETADAS:
-    1_1 Enseñada
-    2_1
-    2_2
+Mision1_1	Testeada, video
+Mision1_2	Testeada, video
+Mision1_3
+
+Mision2_1	Testeada, video
+Mision2_2	Testeada
+
+Mision3_1
+Mision3_2
+
+Mision4
+
+Mision5		Testeada
--- mision4/mision4.py
+++ mision4/mision4.py
@@ -7,6 +7,7 @@
 #!/usr/bin/env python
 
 import nxt.bluesock
+import nxt.locator
 from nxt.sensor import *
 from nxt.motor import *
 import time
@@ -16,15 +17,35 @@ class Robot:
     def __init__(self, brick):
 
         self.brick_= brick
-        self.sensorTouch= Touch(self.brick, PORT_1)
         self.syncMotor_ = SynchronizedMotors(Motor(self.brick_, PORT_B), Motor(self.brick_, PORT_C), 0)
+        self.arm_ = Motor(self.brick_, PORT_A)
         self.sensorUltraSound_= UltraSound(self.brick_, PORT_4)
 
-    # 4. Opcional. Emitir sonido como finalizacion
-    brick.play_tone_and_wait(659, 500)
+        self.separationBetweenWheels_= 14
+        self.cuenta_= ((encoder_*math.pi)/wheelDiameter_)
+        turn_perimeter = (math.pi * self.wheelDiameter_) / 4
+        self.cuentasGiro_ = turn_perimeter / self.cuenta_
+
+    def mision(self):
+
+        #1.Probar que mierdas devuelve cada funcion, falta probar los setters
+        while True:
+            print "Distance: ", self.sensorUltraSound_.get_distance()
+            print "Measurament units: ", self.sensorUltraSound_.get_measurement_units()
+            print "Interval: ", self.sensorUltraSound_.get_interval()
+            print "All measuraments: ", self.sensorUltraSound_.get_all_measurements()
+
+
+        # 3. Parar motores
+        self.syncMotor_.idle()
+
+        # 4. Opcional. Emitir sonido como finalizacion
+        self.brick_.play_tone_and_wait(659, 500)
 
 
 if __name__=='__main__':
-    #robot= Robot(nxt.locator.find_one_brick())
-    robot= Robot(nxt.bluesock.BlueSock('00:16:53:09:46:3B').connect())
+    robot= Robot(nxt.locator.find_one_brick())
+    #robot= Robot(nxt.bluesock.BlueSock('00:16:53:09:46:3B').connect())
     robot.mision()
+
+
