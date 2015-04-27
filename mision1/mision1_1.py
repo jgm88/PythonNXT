f89fe4ca570b0f0a61270fe6ec55689826412ae1
yizu <yizu@blackfingers.local>
2015-04-27 17:22:04 +0200

    Añadido optional

--- mision1/mision1_1.py
+++ mision1/mision1_1.py
@@ -28,10 +28,10 @@ class Robot:
         turn_perimeter = (math.pi * 2.0 * self.separationBetweenWheels_) / 4.0
         self.cuentasGiro_ = turn_perimeter / self.cuenta_
 
-    def moveTurnTime(self,t):
+    def moveTurnTime(self,t, power):
 
         current_time = time.time()
-        self.syncMotor_.run(80)
+        self.syncMotor_.run(80*power)
         while (time.time() - current_time < t):
             pass;      
         self.syncMotor_.brake()
@@ -40,7 +40,7 @@ class Robot:
 
 
         # 1.Mover robot hacia delante 3 segundos
-        self.moveTurnTime(3.0)
+        self.moveTurnTime(3.0,1)
         time.sleep(1)
          
         # 2.Girar a la derecha
@@ -48,7 +48,7 @@ class Robot:
         time.sleep(1)
 
         # 3.Mover robot hacia delante 2 segundos
-        self.moveTurnTime(2.0)
+        self.moveTurnTime(2.0,1)
         time.sleep(1)
 
         # 4. Girar a la izquierda
@@ -56,12 +56,13 @@ class Robot:
         time.sleep(1)
 
         # 5. Moverse hacia delante 3 segundos
-        self.moveTurnTime(3.0)
+        self.moveTurnTime(3.0,1)
+        time.sleep(1)
 
         self.syncMotor_.follower.weak_turn(80, self.cuentasGiro_)
         time.sleep(1)
 
-        self.moveTurnTime(3.0)
+        self.moveTurnTime(3.0,1)
         time.sleep(1)
 
         # 6. Dessincrnizacion motores
@@ -69,3 +70,31 @@ class Robot:
 
         # 7. Emitir sonido de finalizacion
         self.brick_.play_tone_and_wait(659, 500)
+
+    def optional(self):
+
+        self.syncMotor_ = SynchronizedMotors(Motor(self.brick_, PORT_B), Motor(self.brick_, PORT_C), 0)
+
+        self.moveTurnTime(1.0, -1)
+        time.sleep(1)
+
+        self.syncMotor_.follower.weak_turn(-80, self.cuentasGiro_)
+        time.sleep(1)
+
+        self.moveTurnTime(3.0,-1)
+        time.sleep(1)
+
+        self.syncMotor_.follower.weak_turn(-80, self.cuentasGiro_)
+        time.sleep(1)
+
+        self.moveTurnTime(2.0,-1)
+        time.sleep(1)
+
+        self.syncMotor_.leader.weak_turn(-80, self.cuentasGiro_)
+        time.sleep(1)
+
+        self.moveTurnTime(3.0,-1)
+        time.sleep(1)
+
+
+
